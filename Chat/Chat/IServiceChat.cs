1bfef95839a4ae04c26734422474f8857e185590
a-gapanovich <a.gapanovich.09@gmail.com>
2021-10-27 22:40:29 +0300

    first commit

---
+++ Chat/Chat/IServiceChat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Chat
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(IServerChatCallback))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        int Connect(string name);
+
+        [OperationContract]
+        void Disonnect(int id);
+
+        [OperationContract(IsOneWay =true)]
+        int SendMsg(string msg);
+
+
+    }
+
+    public interface IServerChatCallback
+    {
+        [OperationContract]
+        void MsgCallback(string msg);
+    }
+}
---
+++ Chat/Chat/ServiceChat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Transactions;
+
+namespace Chat
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ServiceChat" в коде и файле конфигурации.
+
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+
+        
+        public int Connect(string name)
+        {
+            ServerUser user = 
+        }
+
+        public void Disonnect(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SendMsg(string msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
