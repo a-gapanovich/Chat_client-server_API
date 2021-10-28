c5654f06189ec9cd468f4f0718d77dd15954f413
a-gapanovich <a.gapanovich.09@gmail.com>
2021-10-28 19:53:59 +0300

    ChatHost and ChatClient

--- Chat/ChatClient/MainWindow.xaml.cs
+++ Chat/ChatClient/MainWindow.xaml.cs
@@ -12,17 +12,81 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChatClient.ServiceChat;
 
 namespace ChatClient
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, ServiceChat.IServiceChatCallback
     {
+        bool isConnected = false;
+        ServiceChat.ServiceChatClient client;
+        int ID;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+                ID = client.Connect(tbUserName.Text);
+                tbUserName.IsEnabled = false;
+                bConDicon.Content = "Disconnect";
+                isConnected = true;
+            }
+        }
+
+        void DisConnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disonnect(ID);
+                client = null;
+                tbUserName.IsEnabled = true;
+                bConDicon.Content = "Connect";
+                isConnected = false;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isConnected)
+            {
+                DisConnectUser();
+            }
+            else
+            {
+                ConnectUser();
+            }
+        }
+
+        public void MsgCallback(string msg)
+        {
+            lbChat.Items.Add(msg);
+        }
+
+        private void tbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                if(client != null)
+                {
+                    client.SendMsg(tbMessage.Text, ID);
+                    tbMessage.Text = string.Empty;
+
+                }
+            {
+                
+            }
         }
     }
 }
